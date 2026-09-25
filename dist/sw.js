9be76fcb52121ceba4d4983684b412d4568bf423
produtivoalex <produtivoalex@users.noreply.github.com>
2026-09-25 12:22:32 -0300

    bump PWA cache and refresh service worker

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE='turboquest-v5-dca8fdf';
+const CACHE='turboquest-v6-8e870b4';
 const CORE=['/','/index.html','/manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
