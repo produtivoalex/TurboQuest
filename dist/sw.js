b21e4f76ad7864b2643c0056c75e48399408b3a6
produtivoalex <produtivoalex@users.noreply.github.com>
2026-09-25 10:17:14 -0300

    refresh cache for no-api mobile mode

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE='turboquest-v4-3c8d278';
+const CACHE='turboquest-v5-dca8fdf';
 const CORE=['/','/index.html','/manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
